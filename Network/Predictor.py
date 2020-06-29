ec6d6e52b836416c2d8ef3f7d33ccb24df794dca
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-06-29 23:24:17 +0200

    Update code flow
    There were tons of global variables, so this more properly encapsulates all of them in their respective functions

--- Network/Predictor.py
+++ Network/Predictor.py
@@ -1,91 +1,96 @@
-import tensorflow as tf
-import os
-import sys
-import time
+import argparse
 import pickle
+import sys
+from datetime import datetime
+
 import I2S_Model
 import I2S_evalData
-from datetime import datetime
-import argparse
 import deepsmiles
-
-parser = argparse.ArgumentParser(description="Predicting test images")
-# Input Arguments
-parser.add_argument(
-	'--input',
-	help = 'Enter the input filename',
-	required = True
-	)
-
-args= parser.parse_args()
-
-f = open('Predictions.txt' , 'w')
-sys.stdout = f
-print (datetime.now().strftime('%Y/%m/%d %H:%M:%S'),"Predictions\n\n", flush=True)
-
-tokenizer = pickle.load(open("tokenizer.pkl","rb"))
+import tensorflow as tf
 
 
-#Prediction model parameters
-embedding_dim = 600
-units = 1024
-vocab_size = len(tokenizer.word_index) + 1
+def main():
+    parser = argparse.ArgumentParser(description="Predicting test images")
+    # Input Arguments
+    parser.add_argument(
+        '--input',
+        help='Enter the input filename',
+        required=True
+    )
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+    args = parser.parse_args()
 
-encoder = I2S_Model.CNN_Encoder(embedding_dim)
-decoder = I2S_Model.RNN_Decoder(embedding_dim, units, vocab_size)
+    tokenizer = pickle.load(open("tokenizer.pkl", "rb"))
 
-#Initialize DeepSMILES
-converter = deepsmiles.Converter(rings=True, branches=True)
+    # Prediction model parameters
+    embedding_dim = 600
+    units = 1024
+    vocab_size = len(tokenizer.word_index) + 1
 
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
 
-#Evaluator
-def evaluate(image):
-	maxlength = 74 #should be determined from running the training script
+    encoder = I2S_Model.CNN_Enc(embedding_dim)
+    decoder = I2S_Model.RNN_Dec(embedding_dim, units, vocab_size)
 
-	hidden = decoder.reset_state(batch_size=1)
+    # Initialize DeepSMILES
+    converter = deepsmiles.Converter(rings=True, branches=True)
 
-	temp_input = tf.expand_dims(I2S_evalData.load_image(image)[0], 0)
-	img_tensor_val = I2S_evalData.image_features_extract_model(temp_input)
-	img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+    with open('Predictions.txt', 'w') as f:
+        print(datetime.now().strftime('%Y/%m/%d %H:%M:%S'), "Predictions\n\n", flush=True, file=f)
 
-	features = encoder(img_tensor_val)
+        result = predictor(args.input, encoder=encoder, decoder=decoder, optimizer=optimizer, tokenizer=tokenizer)
 
-	dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
-	result = []
-
-	for i in range(maxlength):
-		predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
-
-		predicted_id = tf.argmax(predictions[0]).numpy()
-		result.append(tokenizer.index_word[predicted_id])
-
-		if tokenizer.index_word[predicted_id] == '<end>':
-			return result
-
-		dec_input = tf.expand_dims([predicted_id], 0)
-
-	return result
+        print(
+            converter.decode(''.join(result).replace("<start>", "").replace("<end>", "")),
+            '\tPredictedSmiles',
+            flush=True,
+            file=f,
+        )
 
 
-#Predictor
-def predictor(Image_path):
-	
-	checkpoint_path = "./Trained_Models"
-	ckpt = tf.train.Checkpoint(encoder=encoder,decoder=decoder,optimizer = optimizer)
-	ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=50)
+# Predictor
+def predictor(path, encoder, decoder, optimizer, tokenizer):
+    checkpoint_path = "./Trained_Models"
+    ckpt = tf.train.Checkpoint(encoder=encoder, decoder=decoder, optimizer=optimizer)
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=50)
 
-	if ckpt_manager.latest_checkpoint:
-		ckpt.restore(tf.train.latest_checkpoint(checkpoint_path))
-		start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(tf.train.latest_checkpoint(checkpoint_path))
+        start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
 
-	result = evaluate(Image_path)
+    result = evaluate(path, encoder=encoder, decoder=decoder, tokenizer=tokenizer)
 
-	return result
+    return result
 
-result = predictor(args.input)
 
-print (converter.decode(''.join(result).replace("<start>","").replace("<end>","")),'\tPredictedSmiles', flush=True)
+# Evaluator
+def evaluate(path, encoder, decoder, tokenizer):
+    maxlength = 74  # should be determined from running the training script
 
-f.close()
+    hidden = decoder.reset_state(batch_size=1)
+
+    temp_input = tf.expand_dims(I2S_evalData.load_image(path)[0], 0)
+    img_tensor_val = I2S_evalData.image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+    features = encoder(img_tensor_val)
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(maxlength):
+        predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result
+
+
+if __name__ == '__main__':
+    main()
